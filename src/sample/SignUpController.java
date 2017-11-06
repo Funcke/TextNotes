a59dc54f4a3226fe9620c91cbaf60c785a10592f
Jonas Funcke <40146720150244@litec.ac.at>
2017-11-06 10:01:21 +0100

    UI update

--- src/sample/SignUpController.java
+++ src/sample/SignUpController.java
@@ -1,8 +1,9 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Window;
 
@@ -10,10 +11,12 @@ import java.sql.*;
 
 public class SignUpController {
     @FXML TextField username;
-    @FXML TextField password;
-    @FXML TextField repeatPassword;
+    @FXML PasswordField password;
+    @FXML TextField passwordVisible;
+    @FXML PasswordField repeatPassword;
+    @FXML TextField passwordRepeatVisible;
     @FXML Label info;
-    @FXML Button check;
+    @FXML CheckBox showPassword;
     private Connection userDB;
     private boolean unused = true;
 
@@ -26,6 +29,7 @@ public class SignUpController {
         try {
             Statement stmt = userDB.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT username FROM users;");
+            String pwordInput = "";
 
             while(rs.next()){
                 if(rs.getString("username").contentEquals(username.getText())) {
@@ -33,10 +37,28 @@ public class SignUpController {
                 }
             }
 
-            if(this.unused && this.password.getText().contentEquals(this.repeatPassword.getText())) {
+            if(showPassword.isSelected()){
+                if(this.unused && this.passwordVisible.getText().contentEquals(this.passwordRepeatVisible.getText())) {
+                    pwordInput = this.passwordVisible.getText();
+                }
+                else{
+                    info.setText("Username already taken or passwords don't match");
+                }
+            }
+            else
+            {
+                if(this.unused && this.password.getText().contentEquals(this.repeatPassword.getText())) {
+                    pwordInput = this.password.getText();
+                }
+                else{
+                    info.setText("Username already taken or passwords don't match");
+                }
+            }
+
+            if(this.unused && (!pwordInput.contentEquals(""))) {
                 PreparedStatement pstmt = userDB.prepareStatement("INSERT INTO users (username, password) VALUES(?, ?)");
                 pstmt.setString(1, username.getText());
-                pstmt.setString(2, password.getText());
+                pstmt.setString(2, pwordInput);
                 pstmt.execute();
                 Window current = this.info.getScene().getWindow();
                 current.hide();
@@ -47,6 +69,28 @@ public class SignUpController {
         }
         catch(SQLException err) {
             System.err.println(err.getMessage());
+        }
+    }
+
+    public void viewPW() {
+        if(showPassword.isSelected()) {
+            this.password.setVisible(false);
+            this.passwordVisible.setVisible(true);
+            this.passwordVisible.setText(password.getText());
+
+            this.repeatPassword.setVisible(false);
+            this.passwordRepeatVisible.setVisible(true);
+            this.passwordRepeatVisible.setText(this.repeatPassword.getText());
+        }
+        else
+        {
+            this.passwordVisible.setVisible(false);
+            this.password.setVisible(true);
+            this.password.setText(this.passwordVisible.getText());
+
+            this.passwordRepeatVisible.setVisible(false);
+            this.repeatPassword.setVisible(true);
+            this.repeatPassword.setText(this.passwordRepeatVisible.getText());
         }
     }
 
